bd3ceb93688deebb76ad49b605978174d331b9a8
Bharat1002 <bharatjc1002@gmail.com>
2022-10-17 20:55:29 +0530

    send messages in room feature

--- src/index.js
+++ src/index.js
@@ -28,8 +28,8 @@ io.on('connection', (socket) => {
 
         socket.join(user.room);
 
-        socket.emit('message', generateMessage('Welcome!'));
-        socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`));
+        socket.emit('message', generateMessage('Admin', 'Welcome!'));
+        socket.broadcast.to(user.room).emit('message', generateMessage('Admin', `${user.username} has joined!`));
         callback();
     })
 
@@ -41,13 +41,13 @@ io.on('connection', (socket) => {
             return callback('Profanity is not allowed!');
         }
 
-        io.to(user.room).emit('message', generateMessage(message));
+        io.to(user.room).emit('message', generateMessage(user.username, message));
         callback();
     });
 
     socket.on('sendLocation', (coords, callback) => {
         const user = getUser(socket.id);
-        io.to(user.room).emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
+        io.to(user.room).emit('locationMessage', generateLocationMessage(user.username, `https://google.com/maps?q=${coords.latitude},${coords.longitude}`));
 
         callback();
     });
@@ -56,7 +56,7 @@ io.on('connection', (socket) => {
         const user = removeUser(socket.id);
 
         if(user){
-            io.to(user.room).emit('message', generateMessage(`${user.username} has left!`));
+            io.to(user.room).emit('message', generateMessage('Admin', `${user.username} has left!`));
         }
 
     });
